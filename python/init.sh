496a6f2dc5fe5c8e1cfa24e6fdb656f2bea840c6
JasonYou <yjj982288470@gmail.com>
2017-02-19 21:38:50 +0800

    vituralenv

--- python/init.sh
+++ python/init.sh
@@ -1,6 +1,11 @@
+# pyenv
 cd
 git clone git://github.com/yyuu/pyenv.git .pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 exec $SHELL
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+sudo apt-get install python-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
