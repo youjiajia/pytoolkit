53d535152176e88408217114ac63498d0b0520bb
JasonYou <yjj982288470@gmail.com>
2017-02-19 21:34:34 +0800

    pyenv

---
+++ python/init.sh
@@ -0,0 +1,6 @@
+cd
+git clone git://github.com/yyuu/pyenv.git .pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+exec $SHELL
