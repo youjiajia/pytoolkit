365ea5fbb8a0761ff7d7b81ed78bdcc25a40d2d7
JasonYou <yjj982288470@gmail.com>
2017-02-19 16:51:31 +0800

    system

--- system/ubuntu_init.sh
+++ system/ubuntu_init.sh
@@ -1,4 +1,13 @@
+"echo 输入你的email"
+read y_email
+"echo 输入你的名称"
+read y_name
 sudo apt-get install git python-pip python-dev libevent-dev build-essential python-setuptools libxml2-dev libxslt-dev openssh-server
-ssh-keygen -t rsa -C “***@gmail.com”
-git config --global user.name "xuhaiyan"
-git config --global user.email "haiyan.xu.vip@gmail.com"
+ssh-keygen -t rsa -C "${y_email}" # ssh配置
+git config --global user.name "${y_name}" # git配置
+git config --global user.email "${y_email}"
+# vim 配置
+echo "set fileencodings=utf-8,gb2312,gbk,gb18030">>~/.vimrc
+echo "set termencoding=utf-8">>~/.vimrc
+echo "set number">>~/.vimrc
+echo "set encoding=prc">>~/.vimrc
