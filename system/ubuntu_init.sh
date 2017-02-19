5148a062dd9ee88ddec4bfd4a7ccc189970193f0
JasonYou <yjj982288470@gmail.com>
2017-02-19 15:47:42 +0800

    系统初始化配置文件

---
+++ system/ubuntu_init.sh
@@ -0,0 +1,4 @@
+sudo apt-get install git python-pip python-dev libevent-dev build-essential python-setuptools libxml2-dev libxslt-dev openssh-server
+ssh-keygen -t rsa -C “***@gmail.com”
+git config --global user.name "xuhaiyan"
+git config --global user.email "haiyan.xu.vip@gmail.com"
