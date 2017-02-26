c3eeb6855caa5cde1673b12962f994dc9c0674d5
JasonYou <yjj982288470@gmail.com>
2017-02-26 15:53:48 +0800

    test

--- script/thunderhelper.py
+++ script/thunderhelper.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import base64
 s = 'QUFlZDJrOi8vfGZpbGV8wsy8/S5BcnJvdy5TMDFFMDEuQ2hpX0VuZy5XRUItSFIuQUMzLjEwMjRYNTc2LngyNjQtWVllVHPIy8jL07DK0y5ta3Z8NTA2MTM1NTI3fDI0ZWEzZGY1OThmYWYxMGQ5YTdkNzA3Yzk4YTM4NDNhfGg9MnQzNmU0YmwyazVwejRneDYycTRiZ2lpdng2b2oyb3F8L1pa'
-
+s = 'QUFodHRwOi8vdnMxMjIuZnJlZS1kdmQ1OC5jb20vob7Dv8jVuPzQwjE1NWR2ZC5jb22hvzIwMTI0NTdzb2UtNzUyLmF2aVpa'
 a = base64.b64encode(s)
 print a
 print type(a)
